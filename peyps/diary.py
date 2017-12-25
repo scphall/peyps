79b2cee849c0a32f8036f783a6fb5a01d874bd5e
scphall <samcphall@gmail.com>
2017-12-25 23:20:45 +0000

    Added utils

--- peyps/diary.py
+++ peyps/diary.py
@@ -1,44 +1,18 @@
 import os
-import textwrap
-from sys import stdout
 import re
+from sys import stdout
 import json
 import uuid
-import pandas as pd
 from collections import defaultdict
 import datetime as dt
 
+import textwrap
+import pandas as pd
+
+from peyps.utils import red, yellow, Names
+
 
 WIDTH = 80
-
-
-RED = u'\u001b[31;1m'
-GREEN = u'\u001b[32;1m'
-YELLOW = u'\u001b[33;1m'
-BLUE = u'\u001b[34;1m'
-MAGENTA = u'\u001b[35;1m'
-CYAN = u'\u001b[36;1m'
-RESET = u'\u001b[0m'
-
-
-class Names(object):
-    date = 'date'
-    time = 'time'
-    tags = 'tags'
-    note = 'note'
-    hash = 'hash'
-
-
-def red(text):
-    return '{}{}{}'.format(RED, text, RESET)
-
-
-def yellow(text):
-    return '{}{}'.format(YELLOW, text)
-
-
-def reset(text):
-    return RESET
 
 
 class Diary(object):
---
+++ peyps/utils.py
@@ -0,0 +1,27 @@
+RED = u'\u001b[31;1m'
+GREEN = u'\u001b[32;1m'
+YELLOW = u'\u001b[33;1m'
+BLUE = u'\u001b[34;1m'
+MAGENTA = u'\u001b[35;1m'
+CYAN = u'\u001b[36;1m'
+RESET = u'\u001b[0m'
+
+
+class Names(object):
+    date = 'date'
+    time = 'time'
+    tags = 'tags'
+    note = 'note'
+    hash = 'hash'
+
+
+def red(text):
+    return '{}{}{}'.format(RED, text, RESET)
+
+
+def yellow(text):
+    return '{}{}'.format(YELLOW, text)
+
+
+def reset(text):
+    return RESET
